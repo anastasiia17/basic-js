dc54c7882ce538695b3a5da6458609b3957dbdc7
anastasiia17 <anastasiia178@gmail.com>
2021-03-07 23:35:08 +0300

    feat: add code to tower, resurcive and chain tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let depth = 1;
+    let tmpCounter = 1;
+    for(let el of arr){
+      if (Array.isArray(el)){
+        tmpCounter = this.calculateDepth(el) + 1;
+      }
+      if (tmpCounter >= depth){
+        depth = tmpCounter;
+      }
+    }
+    return depth;
   }
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [], 
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(String(value))
+    return this;
+    
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(position > 0){
+      this.chain.splice(position-1, 1);
+      return this;
+    }else{
+      this.chain =[];
+      throw new Error('Error');
+    }
+    
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let finish = this.chain.map(function(el){
+      return `( ${el} )`;
+    })
+    this.chain = [];
+    return finish.join('~~');
   }
 };
 
