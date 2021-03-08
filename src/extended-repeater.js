78afb7a02771a96b0aa67ef2518ddea0f1c71c49
anastasiia17 <anastasiia178@gmail.com>
2021-03-08 23:59:07 +0300

    feat: add code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let repeatTimes = options.repeatTimes,
+  separator = options.separator,
+  addition = options.addition,
+  additionRepeatTimes = options.additionRepeatTimes,
+  additionSeparator = options.additionSeparator;
+
+  if (repeatTimes === undefined){
+    repeatTimes = 1;
+  }
+  
+  if (separator === undefined){
+    separator = "+";
+  }
+  
+  if (addition === undefined){
+    addition = "";
+  }
+  
+  if (additionRepeatTimes === undefined){
+    additionRepeatTimes = 1;
+  }
+  
+  if (additionSeparator === undefined){
+    additionSeparator = "|";
+  }
+
+  let additionStr = (addition + additionSeparator).repeat(additionRepeatTimes);
+  let subStr = additionStr.substring(0, String(additionStr).length - String(additionSeparator).length)
+  let Str = str + subStr; 
+  let resultString = (Str + separator).repeat(repeatTimes);
+  return resultString.substring(0, String(resultString).length - String(separator).length);
+
 };
   
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Error');
+  }
+  let transformed = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i++;
+      console.log(i);
+    }
+    else if (arr[i] === '--discard-prev') {
+        transformed.pop();
+    }
+    else if (arr[i] === '--double-next') {
+      if (i != arr.length - 1) {
+        transformed.push(arr[i + 1]);
+      } 
+    }
+    else if (arr[i] === '--double-prev') {
+        transformed.push(transformed[transformed.length - 1]);
+    } else {
+      transformed.push(arr[i]);
+    }
+  }
+  
+  return transformed;
 };
